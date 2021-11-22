e29f674d289dd6cdcdf37f818cba4b3787d776cc
erobert <etiennerobert33@gmail.com>
2021-11-22 15:35:16 +0100

    Add basic nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
     </div>
   );
 }
---
+++ src/Nav.tsx
@@ -0,0 +1,17 @@
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <button>Add Item</button>
+      </li>
+      <li>
+        <button>Create Outfit</button>
+      </li>
+      <li>
+        <button>My clothes</button>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
