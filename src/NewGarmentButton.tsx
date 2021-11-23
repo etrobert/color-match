42f9f20ee97473a22a1fdab5da0e93883c43e482
erobert <etiennerobert33@gmail.com>
2021-11-24 00:49:16 +0100

    Add an invisible file input

--- src/NewGarmentButton.tsx
+++ src/NewGarmentButton.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { useRef } from 'react';
 import FeatherIcon from './FeatherIcon';
 
 import './NewGarmentButton.css';
@@ -7,10 +8,27 @@ type Props = {
   className?: string;
 };
 
-const NewGarmentButton = ({ className }: Props) => (
-  <button className={classNames('NewGarmentButton', className)}>
-    <FeatherIcon name="plus" options={{ width: '2.5rem', height: '2.5rem' }} />
-  </button>
-);
+const NewGarmentButton = ({ className }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+  return (
+    <>
+      <input
+        type="file"
+        accept="image/*"
+        ref={ref}
+        style={{ display: 'none' }}
+      />
+      <button
+        onClick={() => ref.current?.click()}
+        className={classNames('NewGarmentButton', className)}
+      >
+        <FeatherIcon
+          name="plus"
+          options={{ width: '2.5rem', height: '2.5rem' }}
+        />
+      </button>
+    </>
+  );
+};
 
 export default NewGarmentButton;
