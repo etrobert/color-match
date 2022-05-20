7415ac32c5441e39fe90ee8d5d3e04d597c6ae1f
erobert <etiennerobert33@gmail.com>
2022-05-20 22:52:14 +0200

    Clear App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,5 @@
-import { useState } from 'react';
-import { useRecoilValue } from 'recoil';
 import './App.css';
-import { viewState } from './atoms';
-import Nav from './Nav';
-import NewGarment from './NewGarment';
-import NewGarmentButton from './NewGarmentButton';
-import Wardrobe from './Wardrobe';
 
-function App() {
-  const view = useRecoilValue(viewState);
-
-  const [imageData, setImageData] = useState<string>();
-
-  return (
-    <div className="App">
-      {imageData === undefined ? (
-        <>
-          <NewGarmentButton onDone={setImageData} />
-          <Nav />
-          {view === 'outfits' && 'Outfits'}
-          {view === 'wardrobe' && <Wardrobe />}
-        </>
-      ) : (
-        <NewGarment
-          imageData={imageData}
-          onDone={() => setImageData(undefined)}
-        />
-      )}
-    </div>
-  );
-}
+const App = () => <>'Hello World'</>;
 
 export default App;
