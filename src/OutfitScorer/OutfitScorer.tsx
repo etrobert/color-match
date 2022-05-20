fe9362b0a7ea6bc676ec9cb749e79f5c8bc3e926
erobert <etiennerobert33@gmail.com>
2022-05-20 23:38:19 +0200

    Set image width

--- src/OutfitScorer/OutfitScorer.tsx
+++ src/OutfitScorer/OutfitScorer.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import NewOutfitButton from './NewOutfitButton';
 
+import './OutfitScorer.css';
+
 const OutfitScorer = () => {
   const [outfit, setOutfit] = useState<string | null>(null);
 
   return (
     <>
-      {outfit !== null && <img src={outfit} />}
+      {outfit !== null && <img className="OutfitScorer__image" src={outfit} />}
       <NewOutfitButton onDone={setOutfit} />
     </>
   );
