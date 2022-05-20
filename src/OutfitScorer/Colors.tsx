bc3ed5b78d1232cb301a5e3f20932f28b02271e0
erobert <etiennerobert33@gmail.com>
2022-05-21 00:21:28 +0200

    Add an Add button

--- src/OutfitScorer/Colors.tsx
+++ src/OutfitScorer/Colors.tsx
@@ -4,11 +4,14 @@ const Colors = () => {
   const [color, setColor] = useState<string>('#ff00ff');
 
   return (
-    <input
-      type="color"
-      value={color}
-      onChange={(event) => setColor(event.target.value)}
-    />
+    <div className="Colors">
+      <input
+        type="color"
+        value={color}
+        onChange={(event) => setColor(event.target.value)}
+      />
+      <button>Add</button>
+    </div>
   );
 };
 
