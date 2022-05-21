695d9cae6582ebd01d9fa5dded690fe931293455
erobert <etiennerobert33@gmail.com>
2022-05-21 02:16:49 +0200

    Use pairs and percentage score

--- src/OutfitScorer/atoms.ts
+++ src/OutfitScorer/atoms.ts
@@ -1,4 +1,5 @@
 import { atom, selector } from 'recoil';
+import { pairs } from '../utils';
 import colorDistance from './colorDistance';
 
 const average = (array: number[]) =>
@@ -11,13 +12,17 @@ const monochromaticScoreState = selector<number>({
   get: ({ get }) => {
     const colors = get(colorsState);
 
-    if (colors.length === 0) return 0;
+    if (colors.length < 2) return 0;
 
-    const allDistances = colors.map((c1) =>
-      colors.map((c2) => colorDistance(c1, c2))
-    );
+    const colorPairs = pairs(colors);
+    console.log(colorPairs);
+    const distances = colorPairs.map(([c1, c2]) => colorDistance(c1, c2));
 
-    return average(allDistances.flat());
+    // Between 0 (best) and 0.5 (worst)
+    const averageDistance = average(distances);
+
+    // Between 100 (best) and 0 (worst)
+    return 100 - Math.floor((averageDistance * 100) / 0.5);
   },
 });
 
