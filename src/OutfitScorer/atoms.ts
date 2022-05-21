2d719d9e15b4c39efdea2547511943b4bf4683a0
erobert <etiennerobert33@gmail.com>
2022-05-21 02:08:08 +0200

    Update monochromatic score

--- src/OutfitScorer/atoms.ts
+++ src/OutfitScorer/atoms.ts
@@ -1,10 +1,24 @@
 import { atom, selector } from 'recoil';
+import colorDistance from './colorDistance';
+
+const average = (array: number[]) =>
+  array.reduce((a, b) => a + b) / array.length;
 
 const colorsState = atom<string[]>({ key: 'Colors', default: [] });
 
 const monochromaticScoreState = selector<number>({
   key: 'MonochromaticScore',
-  get: () => 0,
+  get: ({ get }) => {
+    const colors = get(colorsState);
+
+    if (colors.length === 0) return 0;
+
+    const allDistances = colors.map((c1) =>
+      colors.map((c2) => colorDistance(c1, c2))
+    );
+
+    return average(allDistances.flat());
+  },
 });
 
 export { colorsState, monochromaticScoreState };
